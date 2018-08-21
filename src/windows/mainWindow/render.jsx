7d52f13d4ff8f084569edd410f867f6dc0e112a4
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-21 13:19:37 +0200

    Move script out of HTML

---
+++ src/windows/mainWindow/render.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+const render = () => {
+  const ResumeContainer = require('../../containers/ResumeContainer').default;
+  const Resume = require('../../components/Resume').default;
+  ReactDOM.render(
+    <AppContainer>
+      <ResumeContainer Resume={Resume} />
+    </AppContainer>,
+    document.getElementById('App')
+  );
+};
+
+render();
+if (module.hot) {
+  module.hot.accept(render);
+}
