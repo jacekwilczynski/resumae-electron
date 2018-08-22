e26236a4a4495169baed183d4fa70d8fb49d569b
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-22 11:27:23 +0200

    Add Reload to dev menu

--- src/windows/mainWindow/menuTemplate.js
+++ src/windows/mainWindow/menuTemplate.js
@@ -41,6 +41,9 @@ const create = ({ runningOnMac, devMode }) => {
           click: (item, focusedWindow) => {
             focusedWindow.toggleDevTools();
           }
+        },
+        {
+          role: 'reload'
         }
       ]
     });
